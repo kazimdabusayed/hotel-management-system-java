34fdb3c600fc012ca7ea306afc40d567f93bec86
kazimdabusayed <kazimohammedabusayed@gmail.com>
2024-05-27 02:55:19 +0600

    Add: Starting Screen

---
+++ src/Classes/StartingScreen.java
@@ -0,0 +1,33 @@
+package Classes;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class StartingScreen extends JFrame {
+
+   StartingScreen() {
+
+      ImageIcon imageIcon = new ImageIcon(ClassLoader.getSystemResource("Icons/StartingScreen.gif"));
+      JLabel label = new JLabel(imageIcon);
+      label.setBounds(0, 0, 1500, 1000);
+      add(label);
+
+      setLayout(null);
+      setLocation(300, 50);
+      setSize(1500, 1200);
+      setVisible(true);
+
+      try {
+         Thread.sleep(5000);
+         setVisible(false);
+
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
+   public static void main(String[] args) {
+      new StartingScreen();
+   }
+}
+
