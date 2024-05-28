fc60fd63df549f606d8924dee0f5c43eaf1d313e
kazimdabusayed <kazimohammedabusayed@gmail.com>
2024-05-29 04:26:13 +0600

    Add: sql file

---
+++ src/sql/holetMS.sql
@@ -0,0 +1,11 @@
+use hotelMS;
+show databases;
+create table login(username varchar(25), password varchar(25));
+select * from login;
+insert into login values('kaziarafat', (12345689));
+create table room(roomnumber varchar(20), availability varchar(20), cleaning_status varchar(20), price varchar(20), bed_type varchar(20));
+select * from room;
+create table employee(name varchar(20), age varchar(20), gender varchar(20), job varchar(20), salary varchar(20), phone varchar(20), email varchar(20), nid varchar(20));
+select * from employee;
+create table driver(name varchar(20), age varchar(20), gender varchar(20), company varchar(20), carname varchar(20), available varchar(20), location varchar(20));
+select * from driver;
