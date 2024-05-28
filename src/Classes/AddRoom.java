5fe393857e2eb3c6ca3b634fe56cc86c88a5ca7c
kazimdabusayed <kazimohammedabusayed@gmail.com>
2024-05-28 20:47:47 +0600

    Add: AddRoom's combobox

--- src/Classes/AddRoom.java
+++ src/Classes/AddRoom.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class AddRoom extends JFrame {
-   JTextField t2;
+   JTextField t2, t4;
 
    AddRoom() {
       // Panel
@@ -34,6 +34,59 @@ public class AddRoom extends JFrame {
       t2.setForeground(Color.WHITE);
       t2.setBackground(new Color(16, 108, 115));
       panel.add(t2);
+
+      JLabel l3 = new JLabel("Availability");
+      l3.setBounds(64, 110, 152, 22);
+      l3.setFont(new Font("Tahoma", Font.BOLD, 14));
+      l3.setForeground(Color.WHITE);
+      panel.add(l3);
+
+      // Combobox
+      JComboBox<String> t3 = new JComboBox<>(new String[] { "Available", "Occupied" });
+      t3.setBounds(200, 110, 156, 20);
+      t3.setFont(new Font("Tahoma", Font.PLAIN, 14));
+      t3.setForeground(Color.WHITE);
+      t3.setBackground(new Color(16, 108, 115));
+      panel.add(t3);
+
+      JLabel l4 = new JLabel("Price");
+      l4.setBounds(64, 150, 152, 22);
+      l4.setFont(new Font("Tahoma", Font.BOLD, 14));
+      l4.setForeground(Color.WHITE);
+      panel.add(l4);
+
+      t4 = new JTextField();
+      t4.setBounds(200, 150, 156, 20);
+      t4.setFont(new Font("Tahoma", Font.PLAIN, 14));
+      t4.setForeground(Color.WHITE);
+      t4.setBackground(new Color(16, 108, 115));
+      panel.add(t4);
+
+      JLabel l5 = new JLabel("Cleaning Status");
+      l5.setBounds(64, 190, 156, 22);
+      l5.setFont(new Font("Tahoma", Font.BOLD, 14));
+      l5.setForeground(Color.WHITE);
+      panel.add(l5);
+
+      JComboBox<String> t5 = new JComboBox<>(new String[] {"Clean", "Dirty"});
+      t5.setBounds(200, 190, 156, 20);
+      t5.setFont(new Font("Tahoma", Font.PLAIN, 14));
+      t5.setForeground(Color.WHITE);
+      t5.setBackground(new Color(16, 108, 115));
+      panel.add(t5);
+
+      JLabel l6 = new JLabel("Bed Type");
+      l6.setBounds(64, 230, 156, 22);
+      l6.setFont(new Font("Tahoma", Font.BOLD, 14));
+      l6.setForeground(Color.WHITE);
+      panel.add(l6);
+
+      JComboBox<String> t6 = new JComboBox<>(new String[] { "Single bed", "Double Bed" });
+      t6.setBounds(200, 232, 156, 20);
+      t6.setFont(new Font("Tahoma", Font.PLAIN, 14));
+      t6.setForeground(Color.WHITE);
+      t6.setBackground(new Color(16, 108, 115));
+      panel.add(t6);
 
       // Frame
       setUndecorated(true);
