01edb4445dc11db75b78286bd612cc7b9e3a7b16
kazimdabusayed <kazimohammedabusayed@gmail.com>
2024-05-28 16:16:49 +0600

    Add: DashBoard functionality

--- src/Classes/DashBoard.java
+++ src/Classes/DashBoard.java
@@ -1,10 +1,35 @@
 package Classes;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
-public class DashBoard extends JFrame{
+public class DashBoard extends JFrame implements ActionListener{
+
+   JButton add, rec;
+
    DashBoard() {
+      super("Hotel Zero Star");
+      // Reception Button
+      rec = new JButton("Reception");
+      rec.setBounds(425, 510, 140, 30);
+      rec.setFont(new Font("Tahoma", Font.BOLD, 15));
+      rec.setBackground(new Color(255, 98, 0));
+      rec.setForeground(Color.white);
+      rec.addActionListener(this);
+      add(rec);
+
+      // Admin button
+      add = new JButton("Admin");
+      add.setBounds(1200, 510, 140, 30);
+      add.setFont(new Font("Tahoma", Font.BOLD, 15));
+      add.setBackground(new Color(255, 98, 0));
+      add.setForeground(Color.white);
+      add.addActionListener(this);
+      add(add);
 
       // Image
       ImageIcon imageIconc = new ImageIcon(ClassLoader.getSystemResource("Icons/reception.png"));
@@ -35,6 +60,18 @@ public class DashBoard extends JFrame{
       setLayout(null);
       setSize(1950, 1090);
       setVisible(true);
+   }
+
+   @Override
+   public void actionPerformed(ActionEvent e) {
+      if (e.getSource() == rec) {
+         new Reception();
+      } else {
+         new LoginPage2();
+         setVisible(false);
+      }
+      // throw new UnsupportedOperationException("Unimplemented method
+      // 'actionPerformed'");
    }
    public static void main(String[] args) {
       new DashBoard();
